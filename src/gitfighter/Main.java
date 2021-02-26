5cbc929d924c536bed3ac9c88e06b08c59a452e5
Dimitrijs Fedotovs <dima@fedoto.ws>
2021-02-26 22:10:45 +0200

    display random numbers

--- src/gitfighter/Main.java
+++ src/gitfighter/Main.java
@@ -1,8 +1,15 @@
 package gitfighter;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        var rand = new Random();
         System.out.println("Hello, World!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(rand.nextInt(100));
+        }
     }
 }
