d6c2939e2dde8c324cc1cf8af5c5f1b4eba52e34
Dimitrijs Fedotovs <dima@fedoto.ws>
2021-02-28 01:19:27 +0200

    squash 1

--- src/gitfighter/Main.java
+++ src/gitfighter/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         var rand = new Random();
         System.out.println("Hello, World!!!");
+        System.out.println("1 - Hello, World!!!");
 
         printNumbers(rand);
     }
